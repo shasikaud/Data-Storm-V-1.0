295ec9639c4af77cfbc28a250ebddf989fa21776
Shasika Udayanga <45961723+shasikaud@users.noreply.github.com>
2020-02-18 00:10:02 +0530

    Update Day 03.py

--- Day 03/Day 03.py
+++ Day 03/Day 03.py
@@ -54,7 +54,7 @@ classifier1.fit(x_train, y_train)
 # Fitting Random Forest Classification to the Training set
 from sklearn.ensemble import RandomForestClassifier
 classifier2 = RandomForestClassifier(n_estimators = 10, criterion = 'entropy', random_state = 0)
-classifier2.fit(X_train, y_train)
+classifier2.fit(x_train, y_train)
 
 
 #Predicting the test set using both classifiers
